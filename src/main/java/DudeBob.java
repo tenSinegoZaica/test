7ee6081033bdddd9afe1be920a3cb56d04dd96f7
L D <tospambox.01@gmail.com>
2017-01-09 23:03:23 +0200

    add Bob

---
+++ src/main/java/DudeBob.java
@@ -0,0 +1,7 @@
+public class DudeBob implements FriendlyDude {
+
+    @Override
+    public void sayHello() {
+        System.out.println("Hey-hey-hey! I am Bob");
+    }
+}
--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -4,8 +4,13 @@ public class Runner {
 
     public static void main(String[] args) {
 
-        FriendlyDude dude = new DudeAlice(15);
-        dude.sayHello();
+        FriendlyDude[] dudes = new FriendlyDude[2];
+        dudes[0] = new DudeAlice(15);
+        dudes[1] = new DudeBob();
+
+        for (FriendlyDude dude : dudes) {
+            dude.sayHello();
+        }
 
     }
 
